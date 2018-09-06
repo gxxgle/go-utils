04228c9dd6beb3128eba3ac1ab83bdd1e58c16cb
hlk <401054117@qq.com>
2018-09-06 19:19:18 +0800

    add schedule struct

--- schedule/schedule.go
+++ schedule/schedule.go
@@ -8,48 +8,84 @@ import (
 	"github.com/robfig/cron"
 )
 
-// global variable
-var (
+type Schedule struct {
 	Cron    *cron.Cron
-	stopped bool
-	wg      sync.WaitGroup
+	Stopped bool
+	Wg      sync.WaitGroup
+}
+
+var (
+	schedule *Schedule
+	err      error
 )
 
 func init() {
-	loc, err := time.LoadLocation("Asia/Shanghai")
+	schedule, err = New()
 	if err != nil {
 		log.Fatalln(err)
 	}
+}
 
-	Cron = cron.NewWithLocation(loc)
-	Cron.Start()
+// New new cron
+func New() (*Schedule, error) {
+	loc, err := time.LoadLocation("Asia/Shanghai")
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+
+	c := cron.NewWithLocation(loc)
+	s := &Schedule{
+		Cron:    c,
+		Stopped: false,
+		Wg:      sync.WaitGroup{},
+	}
+
+	s.Cron.Start()
+	return s, nil
 }
 
 // AddCronFunc [docs](https://godoc.org/github.com/robfig/cron)
 func AddCronFunc(spec string, fn func()) error {
-	return Cron.AddFunc(spec, func() {
-		wg.Add(1)
-		defer wg.Done()
+	return schedule.AddCronFunc(spec, fn)
+}
+
+// AddLoopFunc can loop run task.
+func AddLoopFunc(sleep time.Duration, fn func()) {
+	schedule.AddLoopFunc(sleep, fn)
+}
+
+// Close close cron
+func Close() {
+	schedule.Close()
+}
+
+// AddCronFunc [docs](https://godoc.org/github.com/robfig/cron)
+func (s *Schedule) AddCronFunc(spec string, fn func()) error {
+	return s.Cron.AddFunc(spec, func() {
+		s.Wg.Add(1)
+		defer s.Wg.Done()
 
 		fn()
 	})
 }
 
 // AddLoopFunc can loop run task.
-func AddLoopFunc(sleep time.Duration, fn func()) {
+func (s *Schedule) AddLoopFunc(sleep time.Duration, fn func()) {
 	go func() {
-		for !stopped {
-			wg.Add(1)
+		for !s.Stopped {
+			s.Wg.Add(1)
 			fn()
-			wg.Done()
+			s.Wg.Done()
 
 			time.Sleep(sleep)
 		}
 	}()
 }
 
-func Close() {
-	stopped = true
-	Cron.Stop()
-	wg.Wait()
+// Close close schedule
+func (s *Schedule) Close() {
+	s.Stopped = true
+	s.Cron.Stop()
+	s.Wg.Wait()
 }
