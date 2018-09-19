52b3593c9b1650b782dbceedd7587005730a6261
miaolz123 <miaolizhao@126.com>
2018-09-20 01:40:00 +0800

    feat: add consul

---
+++ consul/consul.go
@@ -0,0 +1,45 @@
+package consul
+
+import (
+	"os"
+
+	"github.com/gxxgle/go-utils/json"
+	"github.com/gxxgle/go-utils/log"
+	"github.com/hashicorp/consul/api"
+)
+
+var Client *api.Client
+
+func init() {
+	var (
+		err    error
+		config = api.DefaultConfig()
+		addr   = os.Getenv("CONSUL_HTTP_ADDR")
+	)
+
+	if addr != "" {
+		config.Address = addr
+	}
+
+	Client, err = api.NewClient(config)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func KVPut(key string, value interface{}) error {
+	_, err := Client.KV().Put(&api.KVPair{
+		Key:   key,
+		Value: json.MustMarshal(value),
+	}, nil)
+	return err
+}
+
+func KVGet(key string, value interface{}) error {
+	pair, _, err := Client.KV().Get(key, nil)
+	if err != nil {
+		return err
+	}
+
+	return json.Unmarshal(pair.Value, value)
+}
